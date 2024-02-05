c12978c92e5821b8725ac39c9310dd0b5a1bee56
m.belogubova <m.belogubova@webvork.com>
2024-02-05 19:32:33 +0300

    feat: implementation for ls

---
+++ src/fileSystem.js
@@ -0,0 +1,18 @@
+import State from './state.js';
+import fs from 'fs/promises'
+export default class FileSystem {
+
+    constructor() {
+        this.stateInstance = new State()
+    }
+
+    async ls() {
+        const fsNodes = await fs.readdir(this.stateInstance.currentDirectory,{withFileTypes: true})
+
+        fsNodes
+            .sort((left,right) => right.isDirectory() - left.isDirectory())
+            .forEach((dirent, index) => {
+                console.log(`${index} - ${dirent.name} - ${dirent.isDirectory() ? 'directory' : 'file' }`)
+            })
+    }
+}
---
+++ src/state.js
@@ -0,0 +1,35 @@
+import path from 'path'
+
+export default class State {
+    static instance
+    _data = {
+        username: '',
+        currentDirectory: ''
+    }
+
+    constructor() {
+        if(State.instance) return State.instance
+        State.instance = this
+    }
+
+    set userName(username) {
+        this._data.username = username
+    }
+
+    get userName() {
+        return this._data.username
+    }
+
+    get currentDirectory() {
+        return this._data.currentDirectory
+    }
+
+    set currentDirectory(currentDirectory) {
+        console.log(currentDirectory, 'current dir ++++++')
+        console.log(typeof currentDirectory)
+        if(typeof currentDirectory === 'string') currentDirectory = path.dirname(currentDirectory)
+        
+        console.log(currentDirectory, 'current dir ++++++')
+        this._data.currentDirectory = currentDirectory
+    }
+}
